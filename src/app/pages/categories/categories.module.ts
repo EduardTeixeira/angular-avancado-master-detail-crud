e1af090f6dbd932f2c92628c6905a6b6db566127
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2020-10-19 23:46:39 -0300

    commit

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
-
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
-  declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
-  ]
+    CategoriesRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [CategoryListComponent, CategoryFormComponent],
 })
 export class CategoriesModule { }
